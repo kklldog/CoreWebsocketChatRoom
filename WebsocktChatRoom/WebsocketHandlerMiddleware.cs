390c55b3d0ad62152ebc6ce96110c27d7c42c11d
kklldog <kklldog@qq.com>
2020-03-03 01:45:06 +0800

    添加项目文件。

---
+++ WebsocktChatRoom/WebsocketHandlerMiddleware.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WebsocktChatRoom
+{
+    public class WebsocketClient
+    {
+        public WebSocket WebSocket { get; set; }
+
+        public string Id { get; set; }
+
+        public string RoomNo { get; set; }
+    }
+
+    public class WebsocketHandlerMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger _logger;
+
+        public WebsocketHandlerMiddleware(
+            RequestDelegate next,
+            ILoggerFactory loggerFactory
+            )
+        {
+            _next = next;
+            _logger = loggerFactory.
+                CreateLogger<WebsocketHandlerMiddleware>();
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            if (context.Request.Path == "/ws")
+            {
+                if (context.WebSockets.IsWebSocketRequest)
+                {
+                    WebSocket webSocket = await context.WebSockets.AcceptWebSocketAsync();
+                    string clientId = Guid.NewGuid().ToString(); ;
+                    var wsClient = new WebsocketClient
+                    {
+                        Id = clientId,
+                        WebSocket = webSocket
+                    };
+                    try
+                    {
+                        await Handle(wsClient);
+                    }
+                    catch (Exception ex)
+                    {
+                        _logger.LogError(ex, "Echo websocket client {0} err .", clientId);
+                        await context.Response.WriteAsync("closed");
+                    }
+                }
+                else
+                {
+                    context.Response.StatusCode = 404;
+                }
+            }
+            else
+            {
+                await _next(context);
+            }
+        }
+
+        private async Task Handle(WebsocketClient webSocket)
+        {
+            var buffer = new byte[1024 * 1];
+            WebSocketReceiveResult result = null;
+            do
+            {
+                result = await webSocket.WebSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+            }
+            while (!result.CloseStatus.HasValue);
+            _logger.LogInformation($"Websocket client closed.");
+        }
+    }
+}
